06454b976b1d465e7a46077f806988672089e63d
John Shin <twglhk@gmail.com>
2025-03-16 20:31:03 +0900

    [Feat] 공통 레이어 스크립트 추가

---
+++ TDS/Assets/2.Scripts/Common/Layers.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace TDS.Common
+{
+    /// <summary>
+    /// 레이어 모음
+    /// </summary>
+    public static class Layers
+    {
+        public static readonly int ZOMBIE = 6;
+        public static readonly int ZOMBIE_MASK = 1 << ZOMBIE;
+    }
+} 
